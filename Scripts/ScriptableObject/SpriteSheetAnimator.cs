aa38ecc905329e59c90e38463eedb20ef6847952
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2022-01-18 01:13:50 +0200

    radius

--- Scripts/ScriptableObject/SpriteSheetAnimator.cs
+++ Scripts/ScriptableObject/SpriteSheetAnimator.cs
@@ -8,6 +8,7 @@ public class SpriteSheetAnimator : ScriptableObject
 {
     public SpriteSheetAnimationScriptable[] animations;
     public int defaultAnimationIndex;
+    public float Radius;
 
     public SpriteSheetAnimationScriptable GetAnimation(Entity renderGroup) => 
         Array.Find(animations, a => a.RenderGroup == renderGroup);
