218a34bea7dbe641bd6d5ff2dd0075dffb225f5f
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2023-03-12 14:26:23 +0200

    new

--- Assets/Runtime/Scripts/Utils/TMUtilsEcs/Float2V.cs
+++ Assets/Runtime/Scripts/Utils/TMUtilsEcs/Float2V.cs
@@ -4,8 +4,8 @@ namespace SmokGnu.SpriteSheetRenderer.Utils.TMUtilsEcs
 {
     public static class Float2V
     {
-        public static float2 One => new float2(1, 1);
-        public static float2 BasisX => new float2(1, 0);
-        public static float2 BasisY => new float2(0, 1);
+        public static float2 One => new(1, 1);
+        public static float2 BasisX => new(1, 0);
+        public static float2 BasisY => new(0, 1);
     }
 }
--- Assets/Runtime/Scripts/Utils/TMUtilsEcs/Float3V.cs
+++ Assets/Runtime/Scripts/Utils/TMUtilsEcs/Float3V.cs
@@ -4,10 +4,10 @@ namespace SmokGnu.SpriteSheetRenderer.Utils.TMUtilsEcs
 {
     public static class Float3V
     {
-        public static float3 One => new float3(1, 1, 1);
+        public static float3 One => new(1, 1, 1);
 
-        public static float3 BasisX => new float3(1, 0, 0);
-        public static float3 BasisY => new float3(0, 1, 0);
-        public static float3 BasisZ => new float3(0, 0, 1);
+        public static float3 BasisX => new(1, 0, 0);
+        public static float3 BasisY => new(0, 1, 0);
+        public static float3 BasisZ => new(0, 0, 1);
     }
 }
